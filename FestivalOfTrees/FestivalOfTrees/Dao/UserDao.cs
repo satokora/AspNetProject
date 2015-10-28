856b40154e3640cb94755be3ec4c94319b504372
TLenahan <tlenahan14@gmail.com>
2015-10-28 15:17:06 -0500

    Add getUserByEmail

---
+++ FestivalOfTrees/FestivalOfTrees/Dao/UserDao.cs
@@ -0,0 +1,18 @@
+﻿using FestivalOfTrees.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace FestivalOfTrees.Dao
+{
+    interface UserDao
+    {
+        User getUserByEmail(string email);
+
+        void getUserByNum(string bidNum);
+
+        User readerToUser(SqlDataReader reader);
+    }
+}
