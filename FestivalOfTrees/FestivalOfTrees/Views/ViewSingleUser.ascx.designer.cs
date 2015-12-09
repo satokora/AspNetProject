2c81e5e94f3e9cbbaf96bc31055b498b1127db8b
satokora <satokorambxl@gmail.com>
2015-12-09 00:21:47 -0600

    designer class upload

--- FestivalOfTrees/FestivalOfTrees/Views/ViewSingleUser.ascx.designer.cs
+++ FestivalOfTrees/FestivalOfTrees/Views/ViewSingleUser.ascx.designer.cs
@@ -121,6 +121,15 @@ namespace FestivalOfTrees.Views {
         protected global::System.Web.UI.WebControls.Label LblEmail;
         
         /// <summary>
+        /// EmailSentMsg control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EmailSentMsg;
+        
+        /// <summary>
         /// InvoiceView control.
         /// </summary>
         /// <remarks>
